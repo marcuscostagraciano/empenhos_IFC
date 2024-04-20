40496ac6bcdbeba97cdbf1af78d4f5e12d14fcca
mateus-lopes <mateusalbano22@gmail.com>
2024-04-20 15:54:39 -0300

    FEAT(layout): Adding new layout

--- utils.py
+++ utils.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import altair as alt
+import streamlit as st
 
 def clean_convert_column(df, column_name):
     """
@@ -22,3 +24,32 @@ def clean_convert_column(df, column_name):
     df[column_name] = pd.to_numeric(df[column_name], errors='coerce')
 
     return df
+
+def create_simple_chart():
+    df = pd.DataFrame({
+        'x': [1, 2, 3, 4, 5],
+        'y': [10, 20, 30, 40, 50]
+    })
+
+    chart = (
+        alt.Chart(df)
+        .mark_line(point=True)
+        .encode(x='x', y='y')
+    )
+
+    return chart
+
+def get_campus_option(id):
+    """
+    This function returns the selected campus option.
+
+    Returns:
+        str: The selected campus option.
+    """
+
+    campus_option = st.selectbox(
+        f"Selecione o Campus {id}",
+        ["Araquari", "Camboriú", "Sombrio", "Videira"],
+    )
+
+    return campus_option
