88d3937281cd29a073d5da354290e49d1a4210a8
mateus-lopes <mateusalbano22@gmail.com>
2024-05-10 16:57:15 -0300

    FEAT: Adding dataframe_manager class

---
+++ classes/dataframe_manager.py
@@ -0,0 +1,37 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import streamlit as st
+from streamlit.logger import get_logger
+
+LOGGER = get_logger(__name__)
+
+class DataframeManager:
+    def __init__(self):
+        self.init_session_state()
+
+    def init_session_state(self):
+        if "error_message" not in st.session_state:
+            st.session_state.error_message = ''
+        if "df_master" not in st.session_state:
+            st.session_state.df_master = pd.read_csv("assets/xls/empenhos.csv", encoding='ISO-8859-1', sep=";", decimal=",")
+    
+    def get_df_month(self):
+        self.to_float()
+        visible_columns = [
+            'Mês',
+            'Empenhado',
+            'Liquidado',
+        ]
+        df = st.session_state.df_master[visible_columns]
+        df_month = df.groupby(['Mês'])[['Empenhado', 'Liquidado']].sum().reset_index()
+        df_month['Empenhado (R$)'] = st.session_state.df_master['Empenhado'].map('R$ {:,.2f}'.format)
+        df_month['Liquidado (R$)'] = st.session_state.df_master['Liquidado'].map('R$ {:,.2f}'.format)
+        
+        return df_month[['Mês', 'Empenhado (R$)', 'Liquidado (R$)']]
+
+    def clean_df(self):
+        pass
+        
+    def to_float(self):
+        st.session_state.df_master['Empenhado'] = st.session_state.df_master['Empenhado'].str.replace('.', '').str.replace(',', '.').astype(float)
+        st.session_state.df_master['Liquidado'] = st.session_state.df_master['Liquidado'].str.replace('.', '').str.replace(',', '.').astype(float)
--- components/main_table.py
+++ components/main_table.py
@@ -1,36 +1,11 @@
 import streamlit as st
 import pandas as pd
-from utils import main_table
+from classes.dataframe_manager import DataframeManager
 
 def main_table():
+    df_manager = DataframeManager()
     campus = 'Araquari'
     st.write(f"## Empenhado x Liquidado (Mês a Mês)")
     st.caption(f"Este gráfico apresenta a evolução dos valores empenhados e liquidados pelo IFC Campus {campus} ao longo dos meses.")
-    
-    df = pd.read_csv("assets/xls/empenhos.csv", encoding='ISO-8859-1', sep=";", decimal=",")
-
-    colunas_visiveis = [
-        'Natureza Despesa',
-        'Natureza Despesa Detalhada',
-        'Métrica',
-        'Mês',
-        'Empenhado',
-        'Liquidado',
-    ]
-
-    df = df[colunas_visiveis]
-    df['Empenhado'] = df['Empenhado'].str.replace('.', '').str.replace(',', '.').astype(float)
-    df['Liquidado'] = df['Liquidado'].str.replace('.', '').str.replace(',', '.').astype(float)
-    df['Empenhado Formatado'] = df['Empenhado'].map('R$ {:,.2f}'.format)
-    df['Liquidado Formatado'] = df['Liquidado'].map('R$ {:,.2f}'.format)
-
-    df_mes = df.groupby(['Mês'])[['Empenhado', 'Liquidado']].sum().reset_index()
-    df_mes["Empenhado Formatado"] = df_mes["Empenhado"].map("R$ {:,.2f}".format)
-    df_mes["Liquidado Formatado"] = df_mes["Liquidado"].map("R$ {:,.2f}".format)
-
-    df_mes_natureza = df.groupby(['Mês', 'Natureza Despesa'])[['Empenhado', 'Liquidado']].sum().reset_index()
-    df_mes_natureza["Empenhado Formatado"] = df_mes_natureza["Empenhado"].map("R$ {:,.2f}".format)
-    df_mes_natureza["Liquidado Formatado"] = df_mes_natureza["Liquidado"].map("R$ {:,.2f}".format)
-
-    df_month = df_mes[['Mês', 'Empenhado Formatado', 'Liquidado Formatado']]
+    df_month = df_manager.get_df_month()
     st.table(df_month)
