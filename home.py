3abd7339f51bce721b2ac0f0f709673da8b5fe27
Marcus Costa Graciano <marcusviniciusgraciano04@gmail.com>
2024-10-06 10:48:04 -0300

    FEAT: add separate function to save timings

--- home.py
+++ home.py
@@ -10,6 +10,40 @@ from components.select_if import select_if
 from components.tabs_childrens import tabs_childrens
 
 
+def save_time_spent_to_csv(
+    select: float,
+    indicators: float,
+    main_charts: float,
+    nature_alls: float,
+    tabs_children: float,
+) -> None:
+    print(f"Tempo gasto com 'select_if()': {select}s")
+    print(f"Tempo gasto com 'indicators()': {indicators}s")
+    print(f"Tempo gasto com 'main_chart()': {main_charts}s")
+    print(f"Tempo gasto com 'nature_all()': {nature_alls}s")
+    print(f"Tempo gasto com 'tabs_childrens()': {tabs_children}s\n")
+
+    with open("tempos_carregamento.csv", "a", newline="\n") as arquivo_tempos:
+        metodos = [
+            "select_if",
+            "indicators",
+            "main_chart",
+            "nature_all",
+            "tabs_childrens",
+        ]
+        writer = csv.DictWriter(arquivo_tempos, fieldnames=metodos, delimiter=",")
+        writer.writerow(
+            {
+                "select_if": select,
+                "indicators": indicators,
+                "main_chart": main_charts,
+                "nature_all": nature_alls,
+                "tabs_childrens": tabs_children,
+            }
+        )
+        print("Dados salvos no CSV!")
+
+
 def main():
     st.title("Acompanhamento da execução orçamentária do :green[IFC - Campus Araquari]")
     st.caption(":blue[Version 1.0.5]")
@@ -17,27 +51,30 @@ def main():
     t1 = perf_counter()
     select_if()
     tempo_select_if = perf_counter() - t1
-    print(f"Tempo gasto com 'select_if()': {tempo_select_if}s")
 
     t1 = perf_counter()
     indicators()
     tempo_indicators = perf_counter() - t1
-    print(f"Tempo gasto com 'indicators()': {tempo_indicators}s")
 
     t1 = perf_counter()
     main_chart()
     tempo_main_chart = perf_counter() - t1
-    print(f"Tempo gasto com 'main_chart()': {tempo_main_chart}s")
 
     t1 = perf_counter()
     nature_all()
     tempo_nature_all = perf_counter() - t1
-    print(f"Tempo gasto com 'nature_all()': {tempo_nature_all}s")
 
     t1 = perf_counter()
     tabs_childrens()
     tempo_tabs_childrens = perf_counter() - t1
-    print(f"Tempo gasto com 'tabs_childrens()': {tempo_tabs_childrens}s\n")
+
+    save_time_spent_to_csv(
+        tempo_select_if,
+        tempo_indicators,
+        tempo_main_chart,
+        tempo_nature_all,
+        tempo_tabs_childrens,
+    )
 
     # REMOÇÃO (TALVEZ) TEMPORÁRIA
     # st.divider()
@@ -54,26 +91,6 @@ def main():
     #     use_container_width=True,
     # ):
     #     st.switch_page("pages/dados_brutos.py")
-
-    with open("tempos_carregamento.csv", "a", newline="\n") as arquivo_tempos:
-        metodos = [
-            "select_if",
-            "indicators",
-            "main_chart",
-            "nature_all",
-            "tabs_childrens",
-        ]
-        writer = csv.DictWriter(arquivo_tempos, fieldnames=metodos, delimiter=",")
-        writer.writerow(
-            {
-                "select_if": tempo_select_if,
-                "indicators": tempo_indicators,
-                "main_chart": tempo_main_chart,
-                "nature_all": tempo_nature_all,
-                "tabs_childrens": tempo_tabs_childrens,
-            }
-        )
-        print("Dados salvos no CSV!")
 
 
 if __name__ == "__main__":
