7408b8b787dadcd69e9b1402e6dbaa191383e804
Marcus Costa Graciano <marcusviniciusgraciano04@gmail.com>
2024-10-06 10:50:27 -0300

    REFACTOR: bunch of QoL changes

--- components/by_month.py
+++ components/by_month.py
@@ -2,23 +2,18 @@ from time import perf_counter
 from typing import Final
 
 import pandas as pd
-import plotly.express as px
 import streamlit as st
 from streamlit_echarts import st_echarts
 
 from classes.dataframe_manager import DataframeManager
-from utils import *
+from utils import formatted_months, get_options_month_detail, unformatted_months
 
 
 def by_month(advanced_report=False):
     df_manager = DataframeManager()
 
-    # start_time = perf_counter()
-    months = [
-        formatted_months(month) for month in st.session_state.df_master["Mês"].unique()
-    ]
-    # stop_time = perf_counter()
-    # print(f"Tempo gasto with MONTHS: {stop_time - start_time}")
+    df_sorted_by_month = st.session_state.df_master.sort_values(by="Mês")
+    months = [formatted_months(month) for month in df_sorted_by_month["Mês"].unique()]
 
     st.multiselect(
         label="Selecione os meses",
@@ -27,7 +22,7 @@ def by_month(advanced_report=False):
         key="months",
         options=months,
         placeholder="Selecione o mês",
-        default=months[-1],
+        default=months[0],
     )
 
     if st.session_state.months == []:
@@ -47,52 +42,26 @@ def by_month(advanced_report=False):
             raw_datas_df = []
 
         st.table(raw_datas_df)
-        row2 = st.columns(2, gap="medium")
+        columns = st.columns(2, gap="medium")
 
-        INDICE_TIPO: Final[dict[int, str]] = {0: "empenhado", 1: "liquidado"}
-        for indice in INDICE_TIPO:
-            with row2[indice]:
-                cria_grafico(df_manager, INDICE_TIPO[indice])
+        GRAPH_TYPES: Final[list[str]] = ["empenhado", "liquidado"]
+        for index, graph_type in enumerate(GRAPH_TYPES):
+            with columns[index]:
+                graph_plot(df_manager, graph_type)
 
 
-def cria_grafico(df_manager: DataframeManager, tipo_dado: str) -> None:
-    tipo_dado_maiusculo: str = tipo_dado.capitalize()
-    st.caption(f"## {tipo_dado_maiusculo}")
+def graph_plot(df_manager: DataframeManager, graph_type: str) -> None:
+    capitalized_graph_type: str = graph_type.capitalize()
+    st.caption(f"## {capitalized_graph_type}")
 
-    st.caption(f"Empenho {tipo_dado} pela Direção do IFC - Araquari")
+    st.caption(f"Empenho {graph_type} pela Direção do IFC - Araquari")
     meses = [unformatted_months(month) for month in st.session_state.months]
 
-    raw_datas = df_manager.get_df_month_monetary_values(meses, tipo_dado_maiusculo)
-    chart_options = get_options_month_detail(raw_datas, tipo_dado_maiusculo)
-
-    # start_time = perf_counter()
-    # APESAR DE FUNCIONAL, O PLOTLY DEMORA MAIS PARA GERAR OS GRÁFICOS 
-    # st.plotly_chart(
-    #     px.pie(
-    #         raw_datas,
-    #         values=tipo_dado_maiusculo,
-    #         names="Natureza Despesa"
-    #         )
-    #     )
-    # fig = px.pie(raw_datas, values=tipo_dado_maiusculo, names="Natureza Despesa")
-
-    # event = st.plotly_chart(fig)
-    # event.selection
+    raw_datas = df_manager.get_df_month_monetary_values(meses, capitalized_graph_type)
+    chart_options = get_options_month_detail(raw_datas, capitalized_graph_type)
 
     st_echarts(
-       chart_options,
-       height="500px",
-       key=f"GRAFICO_{tipo_dado}",
+        chart_options,
+        height="500px",
+        key=f"GRAFICO_{graph_type}",
     )
-
-    # O PROBLEMA ESTÁ NO "key"
-    # st_echarts(
-    #     chart_options,
-    #     height="500px",
-    #     key=f"{st.session_state}df_moth_detail_liquidated",
-    # )
-
-    # stop_time = perf_counter()
-    # print(
-    #     f"Tempo gasto gerando gráfico '{tipo_dado_maiusculo}': {stop_time - start_time}\n"
-    # )
